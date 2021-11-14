9f22a1bd328a401fb4e968d36ace5c189eca8a36
husamkamal <husamkmal94@gmail.com>
2021-11-14 23:20:39 +0200

    change the section taske Relates #20

--- todo.js
+++ todo.js
@@ -19,25 +19,3 @@ function darkModeOn() {
 function darkModeOff() {
     document.body.classList.remove('dark-mode')
 }
-//check all start and visible buttton 
-function check(checked = true) {
-    const cbs = document.querySelectorAll('input[name="tasck"]');
-    cbs.forEach((cb) => {
-        cb.checked = checked;
-    });
-}
-const btn = document.querySelector('#btn-all');
-btn.onclick = checkAll;
-function checkAll() {
-    check();
-    document.querySelector('.del').style.visibility = 'visible';
-
-    this.onclick = uncheckAll;
-}
-function uncheckAll() {
-    check(false);
-    this.onclick = checkAll;
-    document.querySelector('.del').style.visibility = 'hidden';
-
-}
-//check all end and visible buttton
